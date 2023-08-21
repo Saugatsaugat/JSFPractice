244faca6a3e9ca098a895be90acc6d2eae5a89a5
Saugatsaugat <saugatthapa2073@gmail.com>
2023-08-21 13:43:19 +0545

    Filter updated to filter by using excluded pages also.

--- src/main/java/Filter/LoginFilter.java
+++ src/main/java/Filter/LoginFilter.java
@@ -1,6 +1,8 @@
 package Filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -15,8 +17,13 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author saugat
  */
-@WebFilter(urlPatterns = {"/faces/view/AdminUI/*", "/faces/view/FutsalOwnerUI/*"})
+@WebFilter(urlPatterns = {"/faces/view/*"})
 public class LoginFilter implements Filter {
+
+    private static final List<String> EXCLUDED_URL_PATTERNS = Arrays.asList(
+            "/JSFProject-1.0/faces/view/UserUI/Home/home.xhtml",
+            "/JSFProject-1.0/faces/view/FutsalSchedule/futsalScheduleDisplay.xhtml"
+    );
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -26,15 +33,30 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
+        String requestURI = req.getRequestURI();
+
         HttpServletResponse res = (HttpServletResponse) response;
         Long userId = (Long) req.getSession().getAttribute("userId");
 
-        if (userId != null) {
-            chain.doFilter(request, response);
-
-        } else {
-            res.sendRedirect(req.getContextPath() + "/faces/view/UserUI/Home/home.xhtml");
+        Boolean shouldApplyFilter = true;
+        for (String excludedPattern : EXCLUDED_URL_PATTERNS) {
+            if (requestURI.startsWith(excludedPattern)) {
+                shouldApplyFilter = false;
+                break;
+            }
         }
+        if (shouldApplyFilter) {
+
+            if (userId != null) {
+                chain.doFilter(request, response);
+
+            } else {
+                res.sendRedirect(req.getContextPath() + "/faces/view/UserUI/Home/home.xhtml");
+            }
+        } else {
+            chain.doFilter(request, response);
+        }
+
     }
 
     @Override
